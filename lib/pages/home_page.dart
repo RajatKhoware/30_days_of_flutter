be507d5dc12c7e136c62d7ef8df43d22c119d8d8
Rajat Khoware <rajatkhoware2002@gmail.com>
2022-06-19 18:39:51 +0530

    Day 13 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import 'package:flutter_application_1/widgets/item_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    int a = 30;
-    String s = "Codepur";
-    final dummylist = List.generate(17, (index) => CatalogModel.items[0]);
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  int a = 30;
+  String s = "Codepur";
+
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+//Getting data from json file
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummylist.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[0],
-            );
-          },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: CatalogModel.items[0],
+              );
+            },
+          ),
         ),
-      )),
+      ),
       drawer: MyDrawer(),
     );
   }
